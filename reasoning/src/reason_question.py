40b7738082102bef7d867a3f04a0f8444b8e4a44
Floris den Hengst <florisdenhengst@gmail.com>
2024-05-14 12:25:23 +0200

    Add ask query

--- reasoning/src/reason_question.py
+++ reasoning/src/reason_question.py
@@ -1,3 +1,14 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+def get_db_connection(address):
+    """
+    Returns the database connection.
+    """
+    connection = SPARQLWrapper(address)
+    connection.setReturnFormat(JSON)
+    connection.addParameter('Accept', 'application/sparql-results+json')
+    return connection
+
 # Should probably somehow talk to the knowledge graph and get info from there?
 # Return "None" if no question could be formulated (can this even happen?)
 def reason_question() -> dict | None:
@@ -7,43 +18,49 @@ def reason_question() -> dict | None:
 # TODO FdH: make specific to data required for reasoning
 def get_required_facts() -> list:
     return [
-        "", # a fact that is present
-        "", # a fact that is not present
-        "", # a fact that is not present
+        "wine:SchlossRothermelTrochenbierenausleseRiesling wine:hasSugar ?o", # a fact that is present
+        "wine:SchlossRothermelTrochenbierenausleseRiesling wine:hasBody ?o", # a fact that is not present
+        "wine:SchlossRothermelTrochenbierenausleseRiesling wine:locatedIn ?o", # a fact that is not present
     ]
 
-
 # TODO FdH: use suitable data formats for facts and db_connection
-def query_for_presence(fact, db_connection) -> bool:
+def query_for_presence(fact: str, db_connection) -> bool:
     # turn fact into query
+    db_connection.setQuery("""
+    PREFIX wine: <http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine##>
 
+    ASK {
+        {}.
+    }
+    """.format(fact)
+    )
     # query db_connection for presence of fact
+    response = db_connection.query().convert()
 
     # interpret results
-    return True
+    return response['boolean']
 
-
-def get_missing_facts(required_facts: list) -> list:
+def get_missing_facts(repository: str, required_facts: list) -> list:
     """
     Returns the subset of required_facts that are not in the knowledge DB.
     Returns an empty list if all required_facts are in the DB.
     """
     # create DB connection
-    db_connection = get_db_connection()
+    db_connection = get_db_connection("http://knowledge:7200/repositories/{repository}/statements")
     missing_facts = []
     for fact in required_facts:
         if not query_for_presence(fact, db_connection):
             missing_facts += fact
     return missing_facts
 
-def sort_missing_facts(facts: list) -> list:
+def sort_missing_facts(facts: list[str]) -> list:
     """
     Returns list of facts, sorted by order in which the corresponding questions need to be asked.
     """
     # TODO FdH: simple sort that always returns list in same order
     return facts
 
-def rule_based_question() -> dict | None:
+def rule_based_question(repository: str) -> dict | None:
     """
     Formulates a question based on the presence of facts in a predetermined format.
     """
@@ -51,7 +68,7 @@ def rule_based_question() -> dict | None:
     required_facts = get_required_facts()
 
     # get list of required facts that are not in knowledge DB
-    missing_facts = get_missing_facts(required_facts)
+    missing_facts = get_missing_facts(repository, required_facts)
 
     # sort the list of missing facts
     # NOTE: sort all facts instead of selecting a single fact to support combined questions later on
