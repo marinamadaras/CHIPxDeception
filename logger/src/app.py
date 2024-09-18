c0f826a7a211303ed9d18d368ab936345d166e80
Shaad Alaka <shaad1@live.nl>
2024-09-18 11:30:43 +0200

    Fix up the logger module

--- logger/src/app.py
+++ logger/src/app.py
@@ -6,16 +6,20 @@ from datetime import datetime
 app = Flask(__name__)
 app.debug = True
 
-log = []
+log_lines = []
+
 
 @app.route('/')
 def hello():
     return 'Hello, I am the logger module!'
 
+
 @app.route('/log/<line>')
-def log(line):
-    timestamp = datetime.datetime.now().isoformat()
-    log.append({"timestamp": timestamp, "content": line})
+def log(line: str):
+    timestamp = datetime.now().isoformat()
+    log_lines.append({"timestamp": timestamp, "content": line})
+    return "ok"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
