8575c63879b20b32ea4751fc603828b95c33c4cf
Shaad Alaka <shaad1@live.nl>
2024-10-23 12:54:20 +0200

    Add and test POST route for logger and improve formatting + minor fixes

--- logger/app/routes.py
+++ logger/app/routes.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, current_app
-from datetime import datetime
+from flask import Blueprint, current_app, request
 from logging import FileHandler
 
 bp = Blueprint('main', __name__)
@@ -10,17 +9,25 @@ def hello():
     return 'Hello, I am the logger module!'
 
 
-@bp.route('/log/<line>')
+@bp.route('/log/<line>', methods=['GET'])
 def log(line: str):
-    timestamp = datetime.now().isoformat()
-    current_app.logger.info(f'{timestamp}:::{line}')
+    current_app.logger.info(line)
     return f"logged: {line}"
 
 
-@bp.route('/log')
+@bp.route('/log', methods=['GET'])
 def get_log():
     for handler in current_app.logger.handlers:
         if isinstance(handler, FileHandler) and 'chip' in handler.baseFilename:
             with open(handler.baseFilename, 'r') as f:
                 return f.readlines()
     return "No log file found!", 503
+
+
+@bp.route('/log', methods=['POST'])
+def log_post():
+    line = request.form["msg"]
+    service = request.form["service_name"]
+    level = int(request.form["levelno"])
+    current_app.logger.log(level, f"{service} ::: {line}")
+    return f"logged: {line}"
--- logger/app/tests/test_app.py
+++ logger/app/tests/test_app.py
@@ -1,3 +1,6 @@
+from logging import StreamHandler
+
+
 def test_hello(client):
     response = client.get('/')
 
@@ -11,7 +14,17 @@ def test_log(client):
     assert logged_line in response.data
 
 
-def test_get_log(client):
+def test_log_post(client):
+    logged_line = b'blabla'
+    response = client.post(f"/log", data={
+        "msg": logged_line,
+        "service_name": "service"
+    })
+
+    assert logged_line in response.data
+
+
+def test_get_log_file(client):
     logged_line_1 = 'blabla'
     logged_line_2 = 'test test'
     client.get(f"/log/{logged_line_1}")
@@ -21,3 +34,18 @@ def test_get_log(client):
 
     assert logged_line_1 in lines[0]
     assert logged_line_2 in lines[1]
+
+    # Still works after inserting adding a different handler
+    logged_line_3 = 'hurray'
+    handler = StreamHandler()
+    client.application.logger.handlers.insert(0, handler)
+    client.get(f"/log/{logged_line_3}")
+    another_response = client.get("/log")
+    more_lines = another_response.get_json()
+    assert logged_line_3 in more_lines[2]
+
+
+def test_get_log_file_not_exist(client):
+    client.application.logger.handlers = []
+    response = client.get("/log")
+    assert response.status_code == 503
