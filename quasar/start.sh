9270f68d2632a2b213ec11b85e12ea32d7f0b450
Shaad Alaka <shaad1@live.nl>
2025-01-15 18:51:54 +0100

    Fix problem with Quasar front-end not booting correctly

--- quasar/start.sh
+++ quasar/start.sh
@@ -6,6 +6,10 @@ gunicorn -w 4 -b 0.0.0.0:5000 'app:create_app()' --worker-class gevent --reload 
 
 # Start the frontend - for now use dev server
 cd /frontend
+# Very strangely, npm install also needs to be performed here, otherwise 
+# the Quasar 'dev' command will not be recognized. Fortunately, it takes 
+# no time at all, because we already installed the dependencies during building.
+npm install  
 quasar dev &
 
 # Wait for any process to exit
